0022520a35e7417770795be015ea3601106e749f
dinhhthanhh <thanh.nd225670@outlook.com>
2024-12-25 16:45:56 +0700

    update code v2

--- sourcecode/src/application/model/Node.java
+++ sourcecode/src/application/model/Node.java
@@ -1,4 +1,4 @@
-package application;
+package application.model;
 
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -6,19 +6,21 @@ import javafx.scene.text.Text;
 
 import java.util.*;
 
-class Node {
+public class Node {
 
-    double x,y;
+    public double x;
+    public double y;
     String name;
     private boolean visited;
-    private Circle circle;
-    private Text text;
+    private final Circle circle;
+    private final Text text;
     LinkedList<Edge> edges;
 
     Node(double x,double y, String name) {
         circle = new Circle(10);
-        circle.setFill(Color.GAINSBORO);
-        circle.setStroke(Color.CORNFLOWERBLUE);
+        circle.setFill(Color.LIGHTSKYBLUE);
+        circle.setStroke(Color.DODGERBLUE);
+        circle.setPickOnBounds(false);
         text = new Text(name);
         this.x = x;
         this.y = y;
@@ -27,7 +29,23 @@ class Node {
         edges = new LinkedList<>();
     }
 
-    Circle getCircle(double scale) {
+    public LinkedList<Edge> getEdge() {
+        return edges;
+    }
+
+    public double getNodeX() {
+        return x;
+    }
+
+    public double getNodeY() {
+        return y;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Circle getCircle(double scale) {
         circle.setCenterX(x * scale);
         circle.setCenterY(y * scale);
         return circle;
@@ -36,21 +54,21 @@ class Node {
         return circle;
     }
 
-    Text getText() {
+    public Text getText() {
         text.layoutXProperty().bind(circle.centerXProperty().add(-text.getLayoutBounds().getWidth() / 2));
         text.layoutYProperty().bind(circle.centerYProperty().add(5));
         return text;
     }
 
-    boolean isVisited() {
+    public boolean isVisited() {
         return visited;
     }
 
-    void visit() {
+    public void visit() {
         visited = true;
     }
 
-    void unvisited() {
+    public void unvisited() {
         visited = false;
     }
 }
